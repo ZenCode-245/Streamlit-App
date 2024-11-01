06642516ad620680d542b78e5bc7ce31a0b03cc5
00BlackStar <98010380+00BlackStar@users.noreply.github.com>
2024-11-01 12:36:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,36 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("ZenCode Dashbboard 2221111")
-st.write("## ZenCode data analysis Dashboard")
-x = st.text_input("Movie", "Star Wars")
+st.write("ZenCode Dashbboard New Update")
+st.write("## Trend")
 
-if st.button("Click Me"):
-    st.write(f"Your favorite movie is `{x}`")
+# Load the CSV data
+data = pd.read_csv('cpd.csv')
+
+# Group by 'loc_state' and count the total number of rows for each state
+state_counts = data['loc_state'].value_counts().reset_index()
+state_counts.columns = ['loc_state', 'Total Rows']
+
+# Sort the data by 'Total Rows' in ascending order
+state_counts = state_counts.sort_values(by='Total Rows', ascending=True)
+
+# Plot the bar chart
+st.bar_chart(state_counts.set_index("loc_state"))
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("Sentiment")
+
+source = data
+
+st.bar_chart(source, x="Sentiment", y="loc_state", color="loc_state", horizontal=True)
 
 
-data = pd.read_csv("movies.csv")
-st.write(data)
-
-
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["Release Year", "Type", "Genres"])
-
-st.bar_chart(chart_data)
+with col3:
+    st.header("Column 3")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
